8b386917497ada604171769f6506f640962c8b6e
24730237-VoMinhTien <24730237@ms.uit.edu.vn>
2025-08-19 17:43:59 +0700

    Tạo logic.js
    Tạo biến

---
+++ logic.js
@@ -0,0 +1,19 @@
+let blockSize = 25;
+let total_row = 17; //total row number
+let total_col = 17; //total column number
+let board;
+let context;
+
+let snakeX = blockSize * 5;
+let snakeY = blockSize * 5;
+
+// Set the total number of rows and columns
+let speedX = 0;  //speed of snake in x coordinate.
+let speedY = 0;  //speed of snake in Y coordinate.
+
+let snakeBody = [];
+
+let foodX;
+let foodY;
+
+let gameOver = false;
